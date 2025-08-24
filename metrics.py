93b23a329149c18355f5c1e0e193f0cbd2328f9c
nachiketpatil0105 <nachiket1729@gmail.com>
2025-08-24 19:51:00 +0530

    Updated metrics.py

--- metrics.py
+++ metrics.py
@@ -14,21 +14,48 @@ def accuracy(y_hat: pd.Series, y: pd.Series) -> float:
     """
     assert y_hat.size == y.size
     # TODO: Write here
-    pass
+
+    total_correct_pred = (y_hat == y).sum()
+    total_instances = len(y)
+
+    acc = float(total_correct_pred / total_instances)
+    return acc
+
+    # pass
 
 
 def precision(y_hat: pd.Series, y: pd.Series, cls: Union[int, str]) -> float:
     """
     Function to calculate the precision
     """
-    pass
+
+    # Precision = True Positives / (True Positives + False Positives)
+
+    true_pos = ((y_hat == cls) & (y == cls)).sum()
+    pred_pos = (y_hat == cls).sum()
+
+    assert pred_pos # Denominator should not be 0
+
+    pre = float(true_pos / pred_pos)
+    return pre
+    # pass
 
 
 def recall(y_hat: pd.Series, y: pd.Series, cls: Union[int, str]) -> float:
     """
     Function to calculate the recall
     """
-    pass
+
+    # Recall = True Positives / (True Positives + False Negatives)
+
+    true_pos = ((y_hat == cls) & (y == cls)).sum()
+    ground_pos = (y == cls).sum()
+
+    assert ground_pos # Denominator should not be 0
+
+    rec = float(true_pos / ground_pos)
+    return rec
+    # pass
 
 
 def rmse(y_hat: pd.Series, y: pd.Series) -> float:
@@ -36,11 +63,26 @@ def rmse(y_hat: pd.Series, y: pd.Series) -> float:
     Function to calculate the root-mean-squared-error(rmse)
     """
 
-    pass
+    # RMSE = ((1 / n) * Summation((y_pred - y_actual)^2))^0.5
+
+    total_instances = len(y)
+    y_hat_y_2_sum = ((y_hat - y) ** 2).sum()
+
+    root_mean_se = y_hat_y_2_sum / total_instances
+    return float(root_mean_se ** 0.5)
+    # pass
 
 
 def mae(y_hat: pd.Series, y: pd.Series) -> float:
     """
     Function to calculate the mean-absolute-error(mae)
     """
-    pass
+
+    # MAE = Summation(|y_pred - y|) / n
+
+    total_instances = len(y)
+    y_hat_y_sum = (abs(y_hat - y)).sum()
+
+    mean_ae = y_hat_y_sum / total_instances
+    return float(mean_ae)
+    # pass 
